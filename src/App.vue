4e90e1f597ef3eb057f56ec3af56fef41d2184ac
Marjorie Mendoza <marjoriemendoza04@hotmail.com>
2023-11-04 10:46:07 -0600

    comit inicial

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import {FontAwesomeIcon} from "./plugins/fontawesome"</script>
+
+<template>
+<div class="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-500">
+        <div class="p-14 bg-white bg-opacity-80 rounded-lg">
+            <img src="https://img.freepik.com/vector-gratis/lindo-perro-sacando-lengua-ilustracion-icono-dibujos-animados_138676-2709.jpg" alt="Tu Imagen" class="w-32 h-32 mx-auto rounded-full">
+            <h1 class="text-2xl text-center text-gray-800 mt-4">Marjorie</h1>
+            <p class="text-center text-gray-600">Bienvenidos a mi portafolio.</p>
+            <p class="text-center text-gray-800" ><font-awesome-icon :icon="['fas', 'graduation-cap']" /> Formacion academica.</p>
+            <p class="text-center text-gray-800" ><font-awesome-icon :icon="['fas', 'phone']" /> Telefono</p>
+
+            
+        </div>
+    </div>
+  
+</template>
+
---
+++ src/plugins/fontawesome.ts
@@ -0,0 +1,9 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+faGraduationCap,
+faPhone
+}
+from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(faGraduationCap,faPhone)
+export {FontAwesomeIcon}
